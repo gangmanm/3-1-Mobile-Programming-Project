db7baa57605915954e1a6c23c9a6c491bd96bffc
minkyung73 <86233382+minkyung73@users.noreply.github.com>
2022-05-26 15:11:26 +0900

    플랜도 추가
    루틴처럼 체크박스 동적 생성 및 에딧텍스트 값 넘겨옴

--- app/src/main/java/com/example/perpectday/Routine.java
+++ app/src/main/java/com/example/perpectday/Routine.java
@@ -1,7 +1,11 @@
 package com.example.perpectday;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +15,9 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Routine extends AppCompatActivity {
 
@@ -22,17 +29,31 @@ public class Routine extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine);
 
+        //----------routine-------------
         Button_routineAdd = findViewById(R.id.Button_routineAdd);
-        Button_planAdd = findViewById(R.id.Button_planAdd);
-
+        //
         dialog_routineAdd = new Dialog(com.example.perpectday.Routine.this);       // Dialog 초기화
         dialog_routineAdd.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
         dialog_routineAdd.setContentView(R.layout.activity_routine_dialog);
-
+        //
         Button_routineAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDialog01();
+            }
+        });
+
+        //----------plan-------------
+        Button_planAdd = findViewById(R.id.Button_planAdd);
+        //
+        dialog_planAdd = new Dialog(Routine.this);
+        dialog_planAdd.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog_planAdd.setContentView(R.layout.activity_plan_dialog);
+        //
+        Button_planAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog02();
             }
         });
     }
@@ -40,12 +61,12 @@ public class Routine extends AppCompatActivity {
     public void showDialog01(){
         dialog_routineAdd.show(); // 다이얼로그 띄우기
 
-        // OK 버튼
+        //OK 버튼
         Button Button_routineOK = dialog_routineAdd.findViewById(R.id.Button_routineOK);
         Button_routineOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createCheckBox();
+                createCheckBox01();
                 dialog_routineAdd.dismiss();    // 다이얼로그 닫기
             }
         });
@@ -59,10 +80,9 @@ public class Routine extends AppCompatActivity {
                 dialog_routineAdd.dismiss(); // 다이얼로그 닫기
             }
         });
-
     }
 
-    public void createCheckBox(){
+    public void createCheckBox01(){
         LinearLayout ll = findViewById(R.id.LinearLayout_RoutineList);
 
         // 1. 체크박스 객체 생성
@@ -70,6 +90,56 @@ public class Routine extends AppCompatActivity {
 
         // 2. 체크박스에 들어갈 문자 설정
         EditText ee = dialog_routineAdd.findViewById(R.id.EditText_routine);
+        String name = ee.getText().toString();
+        checkboxView.setText(name);
+        checkboxView.setTextSize(20);
+
+        // 3. 체크박스 id 설정
+        checkboxView.setId(0);
+
+        // 4. 레이아웃 설정
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams
+                (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        // 5. 설정한 레이아웃에 체크박스 적용
+        checkboxView.setLayoutParams(param);
+
+        // 6. 생성 및 설정된 체크박스 레이아웃에 적용
+        ll.addView(checkboxView);
+    }
+
+    public void showDialog02(){
+        dialog_planAdd.show();
+
+        //OK 버튼
+        Button Button_planOK = dialog_planAdd.findViewById(R.id.Button_planOK);
+        Button_planOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createCheckBox02();
+                dialog_planAdd.dismiss();    // 다이얼로그 닫기
+            }
+        });
+
+        // Cancel 버튼
+        Button Button_planCancel = dialog_planAdd.findViewById(R.id.Button_planCancel);
+        Button_planCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                dialog_planAdd.dismiss(); // 다이얼로그 닫기
+            }
+        });
+    }
+
+    public void createCheckBox02(){
+        LinearLayout ll = findViewById(R.id.LinearLayout_PlanList);
+
+        // 1. 체크박스 객체 생성
+        CheckBox checkboxView = new CheckBox(getApplicationContext());
+
+        // 2. 체크박스에 들어갈 문자 설정
+        EditText ee = dialog_planAdd.findViewById(R.id.EditText_plan);
         String name = ee.getText().toString();
         checkboxView.setText(name);
         checkboxView.setTextSize(20);
