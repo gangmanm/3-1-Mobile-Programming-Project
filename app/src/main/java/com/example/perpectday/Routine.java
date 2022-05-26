a8de07c00454473ce00205fe2087ce6609d528b2
minkyung73 <86233382+minkyung73@users.noreply.github.com>
2022-05-26 11:34:35 +0900

    다이얼로그
    다이얼로그에 내용 입력하면 체크박스의 텍스트에 들어감

--- app/src/main/java/com/example/perpectday/Routine.java
+++ app/src/main/java/com/example/perpectday/Routine.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class Routine extends AppCompatActivity {
 
@@ -22,7 +25,7 @@ public class Routine extends AppCompatActivity {
         Button_routineAdd = findViewById(R.id.Button_routineAdd);
         Button_planAdd = findViewById(R.id.Button_planAdd);
 
-        dialog_routineAdd = new Dialog(Routine.this);       // Dialog 초기화
+        dialog_routineAdd = new Dialog(com.example.perpectday.Routine.this);       // Dialog 초기화
         dialog_routineAdd.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
         dialog_routineAdd.setContentView(R.layout.activity_routine_dialog);
 
@@ -37,22 +40,13 @@ public class Routine extends AppCompatActivity {
     public void showDialog01(){
         dialog_routineAdd.show(); // 다이얼로그 띄우기
 
-        /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
-
-        // 위젯 연결 방식은 각자 취향대로~
-        // '아래 아니오 버튼'처럼 일반적인 방법대로 연결하면 재사용에 용이하고,
-        // '아래 네 버튼'처럼 바로 연결하면 일회성으로 사용하기 편함.
-        // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
-
         // OK 버튼
         Button Button_routineOK = dialog_routineAdd.findViewById(R.id.Button_routineOK);
         Button_routineOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 원하는 기능 구현
-                //edit text에서 입력받은 값 저장
-                //체크박스 추가하고 거기에 값 할당
-                finish();           // 앱 종료
+                createCheckBox();
+                dialog_routineAdd.dismiss();    // 다이얼로그 닫기
             }
         });
 
@@ -66,5 +60,31 @@ public class Routine extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void createCheckBox(){
+        LinearLayout ll = findViewById(R.id.LinearLayout_RoutineList);
+
+        // 1. 체크박스 객체 생성
+        CheckBox checkboxView = new CheckBox(getApplicationContext());
+
+        // 2. 체크박스에 들어갈 문자 설정
+        EditText ee = dialog_routineAdd.findViewById(R.id.EditText_routine);
+        String name = ee.getText().toString();
+        checkboxView.setText(name);
+        checkboxView.setTextSize(20);
+
+        // 3. 체크박스 id 설정
+        checkboxView.setId(0);
+
+        // 4. 레이아웃 설정
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams
+                (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        // 5. 설정한 레이아웃에 체크박스 적용
+        checkboxView.setLayoutParams(param);
+
+        // 6. 생성 및 설정된 체크박스 레이아웃에 적용
+        ll.addView(checkboxView);
     }
 }
